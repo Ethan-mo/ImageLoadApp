8f5fbe5ec4b7d187f45cc8ad8d7bb689d31e5759
Ethan-mo <ethan.mo@goodmonit.com>
2023-03-02 16:35:34 +0900

    Initial Commit

---
+++ ImageLoadApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLoadApp
+//
+//  Created by 모상현 on 2023/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
